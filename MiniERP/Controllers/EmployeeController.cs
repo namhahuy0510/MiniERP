dbc696672924a521926cec869bd021f8b36d862b
namhahuy0510 <namhahuy0510@gmail.com>
2026-02-23 13:49:16 +0700

    update app

--- MiniERP/Controllers/EmployeeController.cs
+++ MiniERP/Controllers/EmployeeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MiniERP.Data;
+using MiniERP.Models;
 
 namespace MiniERP.Controllers
 {
@@ -16,5 +18,52 @@ namespace MiniERP.Controllers
             var employees = _context.Employees.ToList();
             return View(employees);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Add(emp);
+                _context.SaveChanges();
+                return RedirectToAction("EmployeeManagement");
+            }
+            return View(emp);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var emp = _context.Employees.Find(id);
+            if (emp == null) return NotFound();
+            return View(emp); // tìm Views/Employee/Edit.cshtml
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var emp = _context.Employees.Find(id);
+            if (emp == null) return NotFound();
+            return View(emp); // tìm Views/Employee/Delete.cshtml
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var emp = _context.Employees.Find(id);
+            if (emp == null) return NotFound();
+            _context.Employees.Remove(emp);
+            _context.SaveChanges();
+            return RedirectToAction("EmployeeManagement");
+        }
+
     }
 }
