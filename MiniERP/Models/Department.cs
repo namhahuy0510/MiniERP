af45d23d18e1c2f8b88ee139db9353b74d9db05e
namha <namhahuy0510@gmail.com>
2026-02-23 09:55:52 +0700

    start project

---
+++ MiniERP/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniERP.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DepartmentName { get; set; } = string.Empty;
+
+
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
---
+++ MiniERP/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace MiniERP.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public string Position { get; set; } = string.Empty ;
+        public decimal Salary   { get; set; }
+
+
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; } = new Department();
+
+    }
+}
